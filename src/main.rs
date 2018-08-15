510c483ce5df9bd94d1f08342fbb110346942e90
Fli.* <fmoon@thoughtworks.com>
2018-08-15 16:59:13 +0100

    Fli.* - make an empty box

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+extern crate ggez;
+
+use ggez::conf;
+use ggez::event;
+use ggez::graphics::{self};
+use ggez::{Context, GameResult};
+
+struct MainState {
+    pos_x: f32,
+}
+
+impl MainState {
+    fn new(_ctx: &mut Context) -> GameResult<MainState> {
+        let s = MainState { pos_x: 0.0 };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        graphics::present(ctx);
+        Ok(())
+    }
+}
+
+pub fn main() {
+    let c = conf::Conf::new();
+    let ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
+    let state = &mut MainState::new(ctx).unwrap();
+    event::run(ctx, state).unwrap();
 }
